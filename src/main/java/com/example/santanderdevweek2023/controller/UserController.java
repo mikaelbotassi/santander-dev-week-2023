011354fef41a34e37d263a0583ab7b7be0c3979f
Mikael Botassi <mikaelbotassi@gmail.com>
2023-09-29 21:35:35 -0300

    Feat: Inserting controller layer for use and treating exceptions

---
+++ src/main/java/com/example/santanderdevweek2023/controller/UserController.java
@@ -0,0 +1,36 @@
+package com.example.santanderdevweek2023.controller;
+
+import com.example.santanderdevweek2023.model.User;
+import com.example.santanderdevweek2023.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id){
+        User user = userService.findById(id);
+        return ResponseEntity.ok(user);
+    }
+
+    @PostMapping
+    public ResponseEntity<User> create(@RequestBody User user){
+        User userCreated = userService.create(user);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(userCreated.getId())
+                .toUri();
+        return ResponseEntity.created(location).body(userCreated);
+    }
+
+}
